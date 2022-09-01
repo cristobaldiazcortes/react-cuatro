07254773cfa31801101e45bf93c370f01472d20d
Cristobal Javier Diaz Cortes <cristobaldps4@gmail.com>
2022-09-01 19:36:05 -0400

    segundo commit

--- src/Components/MiApi.jsx
+++ src/Components/MiApi.jsx
@@ -1,45 +1,72 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import {Table} from 'react-bootstrap';
 
-const baseUrl = "https://swapi.dev/api/people/"
-console.log(baseUrl)
+const baseUrl = "https://aves.ninjas.cl/api/birds";
+console.log(baseUrl);
 
 const MiApi = () => {
-    const [datos, setDatos] = useState([]);
-    const[filtro,setFiltro]=useState("");
+  const [datos, setDatos] = useState([]);
+  const [filtro, setFiltro] = useState("");
 
-useEffect(() => {
-        const fetchDatos = async () => {
-            let urlFiltro = baseUrl;
+  useEffect(() => {
+    const fetchDatos = async () => {
+      let urlFiltro = baseUrl;
+      if (filtro !== "") {
+        urlFiltro = baseUrl + "?name=" + filtro;
+      }
 
-            if (filtro !== "") {
-                urlFiltro = baseUrl + "?name=" + filtro;
-            }
+      const response = await fetch(urlFiltro);
+      const respDatos = await response.json();
+      console.log(respDatos);//veo como llega la informacion
+      setDatos(respDatos);
+    };
 
-            const resp = await fetch(urlFiltro);
-            const respDatos = await resp.json();
-            console.log(respDatos);
-            setDatos(respDatos.results);
-        }
+    fetchDatos();
+  }, [filtro]);
 
-        fetchDatos();
-    }, [filtro])
-
- const capturaNombrebuscar=(e)=>{
-        setFiltro(e.target.value)
- }
- return (
+  const capturaNombrebuscar = (e) => {
+    setFiltro(e.target.value);
+  };
+  console.log(capturaNombrebuscar);
+  
+  return (
     <>
-        <h1>FetchDatos</h1>
-        <input value={filtro}
-             onChange={capturaNombrebuscar}></input>
-        <ul>
-            {datos.map((dato) => (
-                <li key={dato.name}>{dato.name}- Porte:{dato.height} </li>
-            ))}
-        </ul>
+      <h1>Aves de Chile</h1>
+      <input value={filtro} onChange={capturaNombrebuscar}></input>
+
+      <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Nombre Latin</th>
+          <th>Imagen</th>
+          
+        </tr>
+      </thead> 
+      <tbody>
+          {datos.map((dato) => ( 
+              <tr key={dato.uid}>
+                  <td>{dato.name.spanish}</td>
+                  <td>{dato.name.latin}</td>
+                  <td><img src={dato.images.main} ></img></td>
+              </tr>
+          ))}  
+      </tbody>
+    </Table>
+
+
+
+
+     {/*  <ul>
+        {datos.map((dato) => (
+          <li key={dato.uid}>
+            Efemeride:{dato.name.spanish}- Fecha:{dato.name.latin} 
+          </li>
+        ))}
+      </ul> */}
     </>
-)
-}
- 
+  );
+};
+
 export default MiApi;
